59abdd14a99a3b6d45c35c8e52bc4d4a8c3880ef
Hawk <97985222+HawkButGithub@users.noreply.github.com>
2023-05-01 22:03:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Client({
   intents: 3276799,
 });
 
+const guildIgnoreList = ["id d'un serveur à ignoré"]
+
 client.on("ready", async () => {
   try {
     const guilds = client.guilds.cache;
@@ -12,71 +14,74 @@ client.on("ready", async () => {
       const automodTypes = [1, 3, 4, 5];
       const promises = [];
 
-      // Loop 7 times for type 1 automod
-      for (let i = 0; i < 7; i++) {
-        promises.push(
-          guild.autoModerationRules
-            .create({
-              name: `Automod by NovaWorld, edited by Hawk.`,
-              creatorId: `820361590826205215`,
-              enabled: true,
-              eventType: 1,
-              triggerType: 1,
-              triggerMetadata: {
-                presets: [1, 2, 3],
-              },
-              actions: [
-                {
-                  type: 1,
-                  metadata: {
-                    channel: guild.channels.cache.first(),
-                    durationSeconds: 10,
-                    customMessage: "Ce message a été bloqué par NovaWorld",
-                  },
-                },
-              ],
-            })
-            .then(() =>
-              console.log(`Automod created in ${guild.name} for type 1`)
-            )
-            .catch((err) =>
-              console.log(`Automod of type 1 already exists in ${guild.name}`)
-            )
-        );
-      }
 
-      // Create other types of automod
-      automodTypes.forEach((type) => {
-        if (type === 1) return;
-        promises.push(
-          guild.autoModerationRules
-            .create({
-              name: `Automod by NovaWorld, edited by Hawk.`,
-              creatorId: `820361590826205215`,
-              enabled: true,
-              eventType: 1,
-              triggerType: type,
-              triggerMetadata: {
-                presets: [1, 2, 3],
-              },
-              actions: [
-                {
-                  type: 1,
-                  metadata: {
-                    channel: guild.channels.cache.first(),
-                    durationSeconds: 10,
-                    customMessage: "Ce message a été bloqué par NovaWorld",
-                  },
+      if(!guildIgnoreList.includes(guild.id)) {
+      // Loop 7 times for type 1 automod
+        for (let i = 0; i < 7; i++) {
+          promises.push(
+            guild.autoModerationRules
+              .create({
+                name: `Automod by NovaWorld, edited by Hawk.`,
+                creatorId: `820361590826205215`,
+                enabled: true,
+                eventType: 1,
+                triggerType: 1,
+                triggerMetadata: {
+                  presets: [1, 2, 3],
                 },
-              ],
-            })
-            .then(() =>
-              console.log(`Automod created in ${guild.name} for type ${type}`)
-            )
-            .catch((err) =>
-              console.log(`Automod of type ${type} already exists in ${guild.name}`)
-            )
-        );
+                actions: [
+                  {
+                    type: 1,
+                    metadata: {
+                      channel: guild.channels.cache.first(),
+                      durationSeconds: 10,
+                      customMessage: "Ce message a été bloqué par NovaWorld",
+                    },
+                  },
+                ],
+              })
+              .then(() =>
+                console.log(`Automod created in ${guild.name} for type 1`)
+              )
+              .catch((err) =>
+                console.log(`Automod of type 1 already exists in ${guild.name}`)
+              )
+          );
+        }
+
+        // Create other types of automod
+        automodTypes.forEach((type) => {
+          if (type === 1) return;
+          promises.push(
+            guild.autoModerationRules
+              .create({
+                name: `Automod by NovaWorld, edited by Hawk.`,
+                creatorId: `820361590826205215`,
+                enabled: true,
+                eventType: 1,
+                triggerType: type,
+                triggerMetadata: {
+                  presets: [1, 2, 3],
+                },
+                actions: [
+                  {
+                    type: 1,
+                    metadata: {
+                      channel: guild.channels.cache.first(),
+                      durationSeconds: 10,
+                      customMessage: "Ce message a été bloqué par NovaWorld",
+                    },
+                  },
+                ],
+              })
+              .then(() =>
+                console.log(`Automod created in ${guild.name} for type ${type}`)
+              )
+              .catch((err) =>
+                console.log(`Automod of type ${type} already exists in ${guild.name}`)
+              )
+          );
+        }
       });
 
       return Promise.all(promises);
